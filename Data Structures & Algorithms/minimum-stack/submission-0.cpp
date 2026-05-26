d2cd366bc9f4a8f1695564be186ce5ebd6369a3e
TGSantpal <santpalkalra1@gmail.com>
2026-05-26 18:16:58 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+private:
+    stack<int> stk;
+    stack<int> minStk;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        if(minStk.empty()){
+            minStk.push(val);
+        }
+        else{
+            minStk.push(min(val, minStk.top()));
+        }
+    }
+    
+    void pop() {
+        stk.pop();
+        minStk.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return minStk.top();
+    }
+};
